b375e98ff1b82a6fe654775d701a0c1da58428a6
programadordelfuturo21 <luis.saravia@globals.one>
2023-07-24 10:24:24 -0500

    prueba de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'new-forms-angular';
+  contentName = '';
+  contentLastName = '';
+  contentCity = '';
+  contentCountry = '';
+  contentEmail = '';
+  contentDNI = '';
+
+  name = '';
+  lastName = '';
+  city = '';
+  country = '';
+  email = '';
+  DNI = '';
+
+  // submit(){
+  //   this.name = this.contentName;
+  //   this.lastName = this.contentLastName;
+  //   this.city = this.contentCity;
+  //   this.country = this.contentCountry;
+  //   this.email = this.contentEmail;
+  //   this.DNI = this.contentDNI;
+  // }
+
+  clear() {
+    this.contentName = '';
+    this.contentLastName = '';
+    this.contentCity = '';
+    this.contentCountry = '';
+    this.contentEmail = '';
+    this.contentDNI = '';
+  }
 }
